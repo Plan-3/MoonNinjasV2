73ac994fae0e1ed2bf0e7fe79eb0ed4dc7f3470e
Victor Perez <victorperez@Victors-MacBook-Pro.local>
2022-07-12 16:42:00 -0600

    front end push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+// Importing react
+import { useState, useEffect } from "react";
+
+// Importing MUI
+import {
+  createTheme,
+  CssBaseline,
+  Grid,
+  ThemeProvider,
+  Typography,
+  Box,
+  Button,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+// Importing font style
+import "./assets/font/blowbrush.ttf";
+import { Container } from "@mui/system";
+
+// Importing test data
+import data from "./assets/temp/data";
+
+const theme = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#ffd400",
+    },
+    secondary: {
+      main: "#40798c",
+    },
+    background: {
+      default: "#ffffff",
+    },
+  },
+  typography: {
+    fontFamily: "BlowBrush",
+  },
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        "@font-face": "BlowBrush",
+      },
+    },
+  },
+});
+
+const MintButton = styled(Button)({
+  padding: "20px 70px",
+  fontSize: "30px",
+  "&:hover": {
+    backgroundColor: "#847E89",
+    color: "#FFFFFF",
+  },
+});
 
 function App() {
+  const [currentImage, setCurrrentImage] = useState(4);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Grid container spacing={2}>
+          {/* This holds the logo at the top */}
+          <Grid item xs={12}>
+            <Typography variant="h1" sx={{ margin: "10px 10px" }}>
+              Moon Ninjas
+            </Typography>
+          </Grid>
+
+          {/* This is the left side box */}
+          <Grid item xs={12} md={7}>
+            <Box
+              sx={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <img className="mainImage" src={data[currentImage].image} />
+            </Box>
+          </Grid>
+
+          {/* This is the right side box */}
+          <Grid item xs={12} md={5}>
+            <Box
+              sx={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography
+                variant="h4"
+                align="center"
+                sx={{
+                  marginBottom: "20px",
+                }}
+              >
+                Use the button below to start making your own ninjas!
+              </Typography>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  marginTop: "auto",
+                  marginBottom: "50px",
+                }}
+              >
+                <MintButton variant="contained">Mint!</MintButton>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ src/assets/temp/data.js
@@ -0,0 +1,33 @@
+import anbuImage from "../images/anbu.png";
+import chuninImage from "../images/chunin.png";
+import geninImage from "../images/genin.png";
+import joninImage from "../images/jonin.png";
+import kageImage from "../images/kage.png";
+import sanninImage from "../images/sannin.png";
+
+export default [
+  {
+    name: "Anub",
+    image: anbuImage,
+  },
+  {
+    name: "Chunin",
+    image: chuninImage,
+  },
+  {
+    name: "Genin",
+    image: geninImage,
+  },
+  {
+    name: "Jonin",
+    image: joninImage,
+  },
+  {
+    name: "Kage",
+    image: kageImage,
+  },
+  {
+    name: "Sannin",
+    image: sanninImage,
+  },
+];
