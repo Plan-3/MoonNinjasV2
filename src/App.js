d395c2b2a2e9e9d8392be51a59e61666710192f6
Victor Perez <victorperez@Victors-MacBook-Pro.local>
2022-07-18 11:32:17 -0600

    added connect button, mobile styling

--- src/App.js
+++ src/App.js
@@ -12,13 +12,14 @@ import {
   Button,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
+import { Container } from "@mui/system";
 
 // Importing font style
 import "./assets/font/blowbrush.ttf";
-import { Container } from "@mui/system";
 
-// Importing test data
+// Importing test data and images
 import data from "./assets/temp/data";
+// import logoImage from "./assets/images/logo3.png";
 
 const theme = createTheme({
   palette: {
@@ -54,6 +55,16 @@ const MintButton = styled(Button)({
   },
 });
 
+const ConnectButton = styled(Button)({
+  padding: "10px 50px",
+  marginTop: "15px",
+  fontSize: "16px",
+  "&:hover": {
+    backgroundColor: "#847E89",
+    color: "#FFFFFF",
+  },
+});
+
 function App() {
   const [currentImage, setCurrrentImage] = useState(4);
 
@@ -61,16 +72,54 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Container maxWidth="lg">
-        <Grid container spacing={2}>
-          {/* This holds the logo at the top */}
-          <Grid item xs={12}>
-            <Typography variant="h1" sx={{ margin: "10px 10px" }}>
-              Moon Ninjas
-            </Typography>
+        <Grid
+          container
+          spacing={2}
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: "100vh" }}
+        >
+          {/* This holds the logo at the top left */}
+          <Grid item xs={12} md={7} order={{ xs: 2, md: 1 }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                height: "200px",
+              }}
+            >
+              {/* Title text logo */}
+              <Typography
+                variant="h1"
+                align="center"
+                sx={{ margin: "10px 10px" }}
+              >
+                Moon Ninjas
+              </Typography>
+              {/* This is the image logo */}
+              {/* <img className="logo" src={logoImage} /> */}
+            </Box>
+          </Grid>
+
+          {/* This hold the connect wallet button top right */}
+          <Grid item xs={12} md={5} order={{ xs: 1, md: 2 }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                height: { xs: "50px", md: "200px" },
+              }}
+            >
+              <ConnectButton variant="contained">
+                Connect Metamask!
+              </ConnectButton>
+            </Box>
           </Grid>
 
           {/* This is the left side box */}
-          <Grid item xs={12} md={7}>
+          <Grid item xs={12} md={7} order={{ xs: 3, md: 3 }}>
             <Box
               sx={{
                 height: "100%",
@@ -84,7 +133,7 @@ function App() {
           </Grid>
 
           {/* This is the right side box */}
-          <Grid item xs={12} md={5}>
+          <Grid item xs={12} md={5} order={{ xs: 4, md: 4 }}>
             <Box
               sx={{
                 height: "100%",
